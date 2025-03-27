f049db6c5ba382b0e2a8da459abcb7dd6dd08324
Kas1es <ananov888egor@gmail.com>
2025-03-27 23:10:58 +0300

    Update firstTest.java

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -3,6 +3,7 @@ import io.qameta.allure.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -19,8 +20,7 @@ public class firstTest {
     @BeforeTest
     public void before() {
         System.setProperty("WebDriver", "C:\\\\Users\\\\Egor\\\\IdeaProjects\\\\Progect\\\\driver\\\\IEDriverServer.exe");
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
+    driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         locators = new Locators(driver);
     }
