e68043e26ba0953d504fb1d81be273ed0932b3ff
Kas1es <ananov888egor@gmail.com>
2025-03-27 23:32:55 +0300

    Update firstTest.java

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions
+import org.openqa.selenium.chrome.ChromeOptions;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
