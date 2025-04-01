33ee91daeadf618f32c1026c0c77be141b2238ce
Kas1es <ananov888egor@gmail.com>
2025-04-01 22:18:52 +0300

    Update firstTest.java

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -34,7 +34,7 @@ public class firstTest {
 
         String selenoidUrl = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         driver = new  RemoteWebDriver(new URL(selenoidUrl), options);
-        String url = System.getProperty("url","https://demoqa.com/automation-practice");
+        String url = System.getProperty("url");
         driver.get(url);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         locators = new Locators(driver);
